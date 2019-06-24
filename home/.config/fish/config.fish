68af58ae36791f120db0b5092f13449485d23462
Robert R. Meyer <blue.dog.archolite@gmail.com>
2019-06-24 16:03:34 -0600

    Changes for nvim configurations for coc

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,3 +2,12 @@
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/home/rmeyer/tools/google-cloud-sdk/path.fish.inc' ]; . '/home/rmeyer/tools/google-cloud-sdk/path.fish.inc'; end
 set -g fish_user_paths "/home/linuxbrew/.linuxbrew/bin" $fish_user_paths
+
+
+# Add Go bin path to path
+set -x PATH $PATH /home/rmeyer/go/bin/
+
+# Enforce editor as nvim
+set -x EDITOR nvim
+
+source ~/.asdf/asdf.fish
