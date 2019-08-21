cc766538f2440bac2ec6c4860aa352ba16129245
Robert R. Meyer <blue.dog.archolite@gmail.com>
2019-08-21 09:48:34 -0600

    Updates for fish configurations

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -3,11 +3,10 @@
 if [ -f '/home/rmeyer/tools/google-cloud-sdk/path.fish.inc' ]; . '/home/rmeyer/tools/google-cloud-sdk/path.fish.inc'; end
 set -g fish_user_paths "/home/linuxbrew/.linuxbrew/bin" $fish_user_paths
 
-
-# Add Go bin path to path
-set -x PATH $PATH /home/rmeyer/go/bin/
-
-# Enforce editor as nvim
 set -x EDITOR nvim
 
+# Fish hook for asdf
 source ~/.asdf/asdf.fish
+
+# Fish hook for direnv
+direnv hook fish | source
